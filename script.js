ad9585bc3c6b06371b266c92b20d5a1f0b2f0d1f
eqea12345 <93880720+eqea12345@users.noreply.github.com>
2024-03-18 19:22:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function playMovie() {
-    var movies = ["The Avengers", "Inception", "The Shawshank Redemption", "Pulp Fiction", "The Dark Knight"];
-    var randomMovie = movies[Math.floor(Math.random() * movies.length)];
-    document.getElementById("movieDisplay").innerHTML = "Playing: " + randomMovie;
+function startGame() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("game").style.display = "block";
+    // You can add additional logic to initialize the game here
+}
+
+function learnMore() {
+    alert("Cinema Manager Game is a tycoon-style game where you manage your own cinema. You'll be able to customize your cinema, schedule movies, set ticket prices, and more. Have fun!");
 }
